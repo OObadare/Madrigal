e072a7ab2e71116e51cb32ffea0f3d5a734e8285
Dare <ogundipe.obadare@gmail.com>
2018-01-02 12:27:19 -0500

    Base display

---
+++ frontend/madrigal.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementByID("root");
+  ReactDOM.render(<h1>Welcome to Madrigal!</h1>, root)
+});
