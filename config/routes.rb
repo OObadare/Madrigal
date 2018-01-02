58aa6ad98adf47f4692d1fa4abefd8ccbf011614
Dare <ogundipe.obadare@gmail.com>
2018-01-02 17:11:12 -0500

    Need to test Ajax requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
-    resource :session, only: [:create, :destroy]
+    resource :session, only: [:create, :destroy, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#root"
