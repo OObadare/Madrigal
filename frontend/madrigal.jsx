3531ec1eecb9d08be2d8b7a89fea2f5d8359b661
Dare <ogundipe.obadare@gmail.com>
2018-01-02 14:13:19 -0500

    Setup complete

--- frontend/madrigal.jsx
+++ frontend/madrigal.jsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React from "react";
 import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementByID("root");
-  ReactDOM.render(<h1>Welcome to Madrigal!</h1>, root)
+const root = document.getElementById('root');
+ReactDOM.render(<h1>Welcome to Madrigal!</h1>, root);
 });
