704d35405d774813210812caa09e98ea33ed443b
Dare <ogundipe.obadare@gmail.com>
2018-01-03 10:24:39 -0500

    Rendering Madrigal from App.jsx

--- frontend/madrigal.jsx
+++ frontend/madrigal.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as SessionApiUtil from "./util/session_api_util";
+import configureStore from './store/store';
+import Root from './components/root';
 
-window.login = SessionApiUtil.login;
-window.logout = SessionApiUtil.logout;
-window.signup = SessionApiUtil.signup;
-document.addEventListener("DOMContentLoaded", () => {
-const root = document.getElementById('root');
-ReactDOM.render(<h1>Welcome to Madrigal!</h1>, root);
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={ store }/>, root);
 });
