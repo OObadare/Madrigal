5512a4e1a7680abe9ff32eb724f56e27c42fee78
Dare <ogundipe.obadare@gmail.com>
2018-01-03 09:32:48 -0500

    Up to errors reducer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
-    resource :user, only: [:create]
+    resources :users, only: [:create]
     resource :session, only: [:create, :destroy, :show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- frontend/madrigal.jsx
+++ frontend/madrigal.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as SessionApiUtil from "./util/session_api_util";
 
+window.login = SessionApiUtil.login;
+window.logout = SessionApiUtil.logout;
+window.signup = SessionApiUtil.signup;
 document.addEventListener("DOMContentLoaded", () => {
 const root = document.getElementById('root');
 ReactDOM.render(<h1>Welcome to Madrigal!</h1>, root);
