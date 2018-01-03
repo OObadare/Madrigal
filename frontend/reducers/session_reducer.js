54fa5b34d98205531647eca44890afb61802392b
Dare <ogundipe.obadare@gmail.com>
2018-01-03 11:43:39 -0500

    logging in from console working

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -4,9 +4,9 @@ import {
   RECEIVE_CURRENT_USER,
 } from '../actions/session_actions';
 
-const _nullUser = Object.freeze({
+const _nullUser = {
   currentUser: null
-});
+};
 
 const sessionReducer = (state = _nullUser, action) => {
   Object.freeze(state);
