23d260d24d3deada89b352e32b0b82a220c19704
Dare <ogundipe.obadare@gmail.com>
2018-01-12 10:29:52 -0500

    changed validation from regex to explicit validations

--- app/models/track.rb
+++ app/models/track.rb
@@ -4,6 +4,6 @@ class Track < ApplicationRecord
 
   has_many :playlists, through: :tracklists
   has_attached_file :song
-  validates_attachment_content_type :song, content_type: /\Aaudio\/.*\z/
+  validates_attachment_content_type :song, content_type: [ 'application/mp3', 'application/x-mp3', 'audio/mpeg', 'audio/mp3' ]
 
 end
